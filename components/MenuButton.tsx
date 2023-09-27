5b30bb3b666156d8b2da8a8efd4f5c95c86d1409
added stat color <littleseon@gmail.com>
2023-09-27 09:37:55 -0400

    Fixed type issue in MenuButton

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,15 +1,15 @@
-
 import React, { useState } from 'react';
-import { IconButton, Menu } from 'react-native-paper';
-import styles from '../AppStyles.js';
+import { Menu } from 'react-native-paper';
 import IconAction from './IconAction.js';
 
 function MenuButton() {
   const [visible, setVisible] = useState(false);
   const toggleMenu = () => setVisible(!visible);
 
-  const handlePress = (item) => {
+  const handlePress = (item: string) => { // Explicitly specify the type here
     toggleMenu();
+    // Do something with the item, e.g., navigate to a different screen
+    console.log(`Selected menu item: ${item}`);
   };
 
   return (
@@ -18,9 +18,18 @@ function MenuButton() {
       onDismiss={toggleMenu}
       anchor={<IconAction icon="menu" action={toggleMenu} />}
     >
-      {/* ...menu items... */}
-      {/* Removed console.log statements */}
+      <Menu.Item onPress={() => handlePress('Home / Dashboard')} title="Home / Dashboard" />
+      <Menu.Item onPress={() => handlePress('Profile / Account')} title="Profile / Account" />
+      <Menu.Item onPress={() => handlePress('Settings')} title="Settings" />
+      <Menu.Item onPress={() => handlePress('Notifications')} title="Notifications" />
+      <Menu.Item onPress={() => handlePress('Help & Support')} title="Help & Support" />
+      <Menu.Item onPress={() => handlePress('About')} title="About" />
+      <Menu.Item onPress={() => handlePress('Features / Scan Tickets / Search')} title="Features / Scan Tickets / Search" />
+      <Menu.Item onPress={() => handlePress('Social')} title="Social" />
+      <Menu.Item onPress={() => handlePress('Logout / Sign Out')} title="Logout / Sign Out" />
+      <Menu.Item onPress={() => handlePress('Tutorials / Walkthrough')} title="Tutorials / Walkthrough" />
     </Menu>
   );
 }
+
 export default MenuButton;
